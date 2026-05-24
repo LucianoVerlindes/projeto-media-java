44bcedd0525159e6fdbebb3186e2b48d36778da9
luciano <verlindes01@gmail.com>
2026-05-24 15:57:30 -0300

    codigo novo

---
+++ CalculadoraMedia.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class CalculadoraMedia {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String nome;
+        double nota1;
+        double nota2;
+
+        System.out.println("Qual seu nome: ");
+        nome = sc.nextLine();
+
+        System.out.println("Qual o valor da nota 1: ");
+        nota1 = sc.nextDouble();
+
+        System.out.println("Qual o valor da nota 2: ");
+        nota2 = sc.nextDouble();
+    }
+}
